c3998b3fe79eccff778a3ba1d19191d82aefce74
maisovoya <katiemeow@mail.ru>
2025-03-11 19:15:49 +0300

    db

--- prs_project/settings.py
+++ prs_project/settings.py
@@ -67,23 +67,18 @@ WSGI_APPLICATION = 'prs_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moviegeek',                      
+        'USER': 'student',
+        'PASSWORD': '1234',
+        "HOST": "85.192.26.208",
+        'PORT': '5432',
+    }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'moviegeek',
-#         'USER': 'postgres',
-#         'PASSWORD': 'hullo1!',
-#         'HOST': 'localhost',
-#         'PORT': 5432
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
