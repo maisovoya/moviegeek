62ab4e8ca73819d885366fc25c80b72b2e1aa5e4
maisovoya <katiemeow@mail.ru>
2025-05-20 19:56:13 +0300

    матричная факторизация + переход на sqlite

--- prs_project/settings.py
+++ prs_project/settings.py
@@ -76,15 +76,21 @@ WSGI_APPLICATION = 'prs_project.wsgi.application'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config["DB_NAME"],                      
-        'USER': config["DB_USER"],
-        'PASSWORD': config["DB_PASSWORD"],
-        "HOST": config["DB_HOST"],
-        'PORT': config["DB_PORT"],
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config["DB_NAME"],                      
+#         'USER': config["DB_USER"],
+#         'PASSWORD': config["DB_PASSWORD"],
+#         "HOST": config["DB_HOST"],
+#         'PORT': config["DB_PORT"],
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
